ede81c87aee54dfbbf36f6bb35a5b74bf2e59856
ianroller4 <ianroller4@gmail.com>
2023-11-06 12:13:07 -0600

    Improved Movement

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -4,27 +4,73 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed = 5f;
+    public float crouchSpeed = 2f;
+    public float walkSpeed = 5f;
     public float sprintSpeed = 10f;
-
-    float speedX;
-    float speedY;
-
+    float currSpeed;
+    bool isCrouching;
     Rigidbody2D rb;
 
-    // Start is called before the first frame update
-    void Start() {
-
+    private void Awake () {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    private void Start() {
         
     }
 
     // Update is called once per frame
-    void Update() {
+    private void Update() {
 
-        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
-        rb.velocity = new Vector2(speedX, speedY);
+    }
+
+    private void FixedUpdate () {
+        Crouch();
+        Move();
+    }
+
+    private void Move () {
+
+        Vector2 moveVec = GetMoveDir();
+
+        MoveSpeed();
+
+        rb.velocity = moveVec * currSpeed;
+    }
+
+    private Vector2 GetMoveDir () {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        return new Vector2(x, y).normalized;
+
+    }
+
+    private void MoveSpeed () {
+
+        if (!isCrouching) {
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                currSpeed = sprintSpeed;
+            } else {
+                currSpeed = walkSpeed;
+            }
+        } else {
+            currSpeed = crouchSpeed;
+        }
+
+    }
+
+    private void Crouch () {
+
+        if (Input.GetKey(KeyCode.Space)) {
+            transform.localScale = new Vector3(1f, 0.5f, 1f);
+            isCrouching = true;
+        } else {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            isCrouching = false;
+        }
 
     }
 }
